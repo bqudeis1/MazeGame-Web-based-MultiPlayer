951f88dc9efddb5fa8d3d8831258903ba0163e75
baha <bahapda@hotmail.com>
2021-02-18 19:29:46 +0200

    dont add me

---
+++ target/WNA_MazeGame_Online-1.0-SNAPSHOT/buttons.js
@@ -0,0 +1,10 @@
+function viewPlayerCommands() {
+    var turnLeft=document.getElementById("TurnLeftCommand");
+    if (turnLeft.style.display === "none") {
+        turnLeft.style.display = "block";
+    } else {
+        turnLeft.style.display = "none";
+    }
+
+
+}
